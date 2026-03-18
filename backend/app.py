c2d5c94b8274ed2db1f5045837ec42736f55df19
victorap-tech <victorapostoles@gmail.com>
2026-03-18 17:39:57 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -16,12 +16,7 @@ CORS(app)
 
 # ── DATABASE ─────────────────────────────────────────────
 database_url = os.environ.get("DATABASE_URL", "")
-
-if database_url.startswith("postgres://"):
-    database_url = database_url.replace(
-        "postgres://", "postgresql+psycopg://", 1
-    )
-
+    
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_POOL_PRE_PING"] = True
