043d621a84030ce92f4502fd6eb355adc0cf6e0b
victorap-tech <victorapostoles@gmail.com>
2026-03-19 20:46:03 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import AdminAgenda from "./pages/AdminAgenda";
 import AdminNuevoTurno from "./pages/AdminNuevoTurno";
 import AdminConfig from "./pages/AdminConfig";
+import AdminDisponibilidad from "./pages/AdminDisponibilidad";
 import Reservar from "./pages/Reservar";
 
 function PrivateRoute({ children }) {
@@ -14,22 +15,19 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Público */}
         <Route path="/" element={<Reservar />} />
         <Route path="/login" element={<Login />} />
-
-        {/* Admin con header */}
         <Route path="/admin/*" element={
           <PrivateRoute>
             <AdminHeader />
             <Routes>
               <Route index element={<AdminAgenda />} />
               <Route path="nuevo" element={<AdminNuevoTurno />} />
+              <Route path="disponibilidad" element={<AdminDisponibilidad />} />
               <Route path="config" element={<AdminConfig />} />
             </Routes>
           </PrivateRoute>
         } />
-
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
